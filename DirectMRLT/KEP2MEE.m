398cf1e33bfc811d50d861ad79e088b956ebaf7f
Yuri <yuri.shimane@gmail.com>
2024-06-20 14:04:04 +0200

    Update KEP2MEE.m

--- DirectMRLT/KEP2MEE.m
+++ DirectMRLT/KEP2MEE.m
@@ -1,3 +1,10 @@
+%% Convert Keplerian elements to Modified Equinoctial Elements
+%
+% Yuri Shimane, yuri.shimane@gatech.edu
+%   Created    : 2024/06/20
+%   Last edits : 2024/06/20
+%
+%
 function MEE = KEP2MEE(KEP)
 a = KEP(:,1);
 e = KEP(:,2);
@@ -9,8 +16,8 @@ TA = KEP(:,6);
 p = a .* (1 - e.^2);
 f = e .* cos(w + W);
 g = e .* sin(w + W);
-h = tan(i/2) .* sin(W);
-k = tan(i/2) .* cos(W);
+h = tan(i/2) .* cos(W);
+k = tan(i/2) .* sin(W);
 L = W + w + TA;
 MEE = [p f g h k L];
 end
